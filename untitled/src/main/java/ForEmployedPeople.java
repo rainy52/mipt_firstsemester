002210a4d65af9e4ad114eb347af17b8fd856eec
Fokin VN <fokin.vn@phystech.edu>
2025-09-18 19:57:09 +0300

    MainClass, ForEmployedPeople, ForStudents, Human

---
+++ untitled/src/main/java/ForEmployedPeople.java
@@ -0,0 +1,9 @@
+public class ForEmployedPeople {
+    public void work (int num){
+    }
+
+    public boolean goHome (String first, String second){
+        return first.equals(second);
+    }
+
+}
